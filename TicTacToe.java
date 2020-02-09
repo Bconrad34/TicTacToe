7c46d94e01ea621c5290170c15c11adb123097df
Bconrad34 <bconrad1217@gmail.com>
2020-02-08 19:00:35 -0500

    first commit

---
+++ TicTacToe.java
@@ -0,0 +1,12 @@
+public class TicTacToe {
+
+    private int[][] board = new int[3][3];
+
+    public static void main (String[] args){
+        
+    }
+
+    public static void updateBoard(){
+
+    }
+}
