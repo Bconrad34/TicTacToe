31b110a8e94b0587cf73bc3040d4cb95428f649d
Bconrad34 <bconrad1217@gmail.com>
2020-02-09 21:14:26 -0500

    added new comments

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,34 +1,44 @@
 import java.util.Scanner;
+//this program creates a simple two player tictactoe game
 public class TicTacToe {
 
+    //create variables, one for the board matrix and one for the counter to see if the game is a tie
     private static char[][] board = new char[3][3];
     private static int takenSpotsCounter = 0;
 
     public static void main (String[] args){
         Scanner in = new Scanner(System.in);
+        //call method to start game
         initializeGame();
+
         while (true){
+            //we want to print the board right before each turn
             updateBoardOutput();
+            //have the player 1 choose the spot on the board, and then update the board
             System.out.println("\nPlayer 1 (O):");
             System.out.println("Row: ");
             int x1 = in.nextInt();
             System.out.println("Column: ");
             int y1 = in.nextInt();
             int check1 = updateBoardValues(x1, y1, 1);
+            //after every choice, check to see if the player has won, if so, break loop
             if (isWin()==1){
                 System.out.println("Congratulations! Player 1 wins!");
                 updateBoardOutput();
                 break;
             }
+            //also need to check if all spots on the board are filled, if so, tie
             else if (takenSpotsCounter==9){
                 System.out.println("It's a draw!");
                 updateBoardOutput();
                 break;
             }
+            //also need to check if the player is trying to choose a spot that is already taken, if so, penalize the player
             if (check1 != 1){
                 System.out.println("Spot taken! You lose your turn.");
                 System.out.println("Pay attention next time.");
             }
+            //repeat for player 2
             updateBoardOutput();
             System.out.println("\nPlayer 2 (X):");
             System.out.println("Row: ");
@@ -55,6 +65,7 @@ public class TicTacToe {
         }
     }
 
+    //this function will display the directions for the game
     public static void initializeGame(){
         System.out.println("\nWelcome to Tic Tac Toe!");
         System.out.println("Directions:");
@@ -64,6 +75,7 @@ public class TicTacToe {
         System.out.println("Good Luck!!!");
     }
 
+    //this function updates the board matrix, also updating the takenSpotsCounter if a new position was filled
     public static int updateBoardValues(int x, int y, int player){
         if (board[x][y] == 'O' || board[x][y] == 'X'){
             return 0;
@@ -79,12 +91,14 @@ public class TicTacToe {
             return 1;
     }
 
+    //this function prints the current board to the screen, making it look like a real tictactoe board
     public static void updateBoardOutput(){
         for (int i = 0; i < 3; i++){
             System.out.println("| "+board[i][0] + " | "+board[i][1]+" | "+board[i][2] + " |");
        }
     }
 
+    //this function checks te possible winning combinations to see if the board contains a winning combination
     public static int isWin(){
         if ((board[0][0] == board[1][1]) && (board[0][0] == board[2][2]) && (board[1][1]==board[2][2]) && board[0][0] != 0){
             return 1;
